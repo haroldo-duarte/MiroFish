962e21a11e3d30e23c7360ac097bffc9b10274a3
haroldo-duarte <hpeslo@gmail.com>
2026-09-21 13:16:04 -0300

    research-lab: link Research Lab from home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,6 +4,7 @@
     <nav class="navbar">
       <div class="nav-brand">MIROFISH</div>
       <div class="nav-links">
+        <router-link to="/research" class="github-link">TOYT RESEARCH LAB <span class="arrow">→</span></router-link>
         <LanguageSwitcher />
         <a href="https://github.com/666ghj/MiroFish" target="_blank" class="github-link">
           {{ $t('nav.visitGithub') }} <span class="arrow">↗</span>
